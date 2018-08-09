ee33ec58b397c9003ced321a4d44085c49daa958
Tman1677 <5393759+Tman1677@users.noreply.github.com>
2018-08-08 21:38:35 -0400

    establish scss

--- compileSass.py
+++ compileSass.py
@@ -1,6 +1,5 @@
+#!/usr/bin/env python
 import sass
 import os
-sass.compile(dirname=('bootstrap-4.1.3/scss', 'css-min/bootstrap'), output_style='compressed')
+sass.compile(dirname=('bootstrap-4.1.3/scss', 'css-min'), output_style='compressed')
 sass.compile(dirname=('scss', 'css-min'), output_style='compressed')
-os.remove('css-min/bootstrap/bootstrap-grid.css')
-os.remove('css-min/bootstrap/bootstrap-reboot.css')
