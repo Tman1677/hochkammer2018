13afc9ae76b1f360ecb5b5a01dd751e4d7eba1cd
Tman1677 <5393759+Tman1677@users.noreply.github.com>
2018-08-09 17:54:10 -0400

    basic outline finished

--- compileSass.py
+++ compileSass.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 import sass
 import os
-sass.compile(dirname=('bootstrap-4.1.3/scss', 'css-min'), output_style='compressed')
+sass.compile(dirname=('bootstrap-4.1.3/scss', 'css-min'))
 sass.compile(dirname=('scss', 'css-min'), output_style='compressed')
